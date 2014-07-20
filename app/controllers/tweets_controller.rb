058806812c3020547d4491fa53772a07c2c5846b
raorao <savinirs@gmail.com>
2014-07-20 14:40:34 -0700

    refactors logic into model

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,13 +1,14 @@
 class TweetsController < ApplicationController
   def recent
-    tweets = Tweet.order("created_at DESC").limit(50).to_json(methods: :hashtag_names)
+    Tweet.ordered_json
+    tweets = Tweet.ordered_json
     render json: tweets
   end
 
   def search
     hashtag = Hashtag.where(name: params[:keyword]).first
     if hashtag
-      render json: hashtag.tweets.order("created_at DESC").limit(50).to_json(methods: :hashtag_names)
+      render json: hashtag.tweets.ordered_json
     else
       render :nothing => true, status: 404
     end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -6,4 +6,8 @@ class Tweet < ActiveRecord::Base
   def hashtag_names
     self.hashtags.pluck(:name)
   end
+
+  def self.ordered_json
+    order("created_at DESC").limit(50).to_json(methods: :hashtag_names)
+  end
 end
