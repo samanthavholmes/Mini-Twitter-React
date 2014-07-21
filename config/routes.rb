a303c114d2f3c89514ccedf1f889698a0ade6219
raorao <savinirs@gmail.com>
2014-07-21 10:42:25 -0700

    generates jasmine install
    adds example file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 TwitterDashboardApi::Application.routes.draw do
+  mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
+
   get 'tweets/recent', to: 'tweets#recent'
   get 'tweets/search/:keyword', to: 'tweets#search'
   post 'tweets', to: 'tweets#create'
